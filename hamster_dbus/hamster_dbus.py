7f2c5d113635ae335c0e1363c2c95ba641095c21
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2016-05-02 12:06:50 +0200

    Provide a rough pytest based setup.
    This works by running the service as a stand alone script via Popen.

--- hamster_dbus/hamster_dbus.py
+++ hamster_dbus/hamster_dbus.py
@@ -24,23 +24,117 @@ import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 import sys
 import datetime
+import logging
 
 import hamsterlib
+from hamsterlib import Category, Activity, Fact
+from six import text_type
+
+logger = logging.getLogger('hamster-dbus')
+
+
+DBUS_SERVICE_NAME = 'org.gnome.hamster_dbus'
+DBUS_OBJECT_PATH = '/org/gnome/hamster_dbus'
+
+# setup basic logging
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 
 class HamsterDBusService(dbus.service.Object):
     """A session bus providing access to hamsterlib."""
 
-    def __init__(self, *args, **kwargs):
-        bus_name = dbus.service.BusName('org.gnome.hamster_dbus', bus=dbus.SessionBus())
-        bus_path = '/org/gnome/hamster_dbus'
-        #super(HamsterDBusService, self).__init__(bus_name, bus_path, *args, **kwargs)
-        #dbus_service = HamsterDBusService(bus_name, bus_path)
-        dbus.service.Object.__init__(self, bus_name, bus_path, *args, **kwargs)
+    def __init__(self, name=None, path=None, bus=None, loop=None):
+        self.loop = loop
+        self.dbus_name = name or DBUS_SERVICE_NAME
+        self.dbus_path = path or DBUS_OBJECT_PATH
+        self.bus = dbus.SessionBus()
+
+        bus_name = dbus.service.BusName(name=self.dbus_name, bus=self.bus)
+        super(HamsterDBusService, self).__init__(bus_name, self.dbus_path)
 
         self.controler = hamsterlib.HamsterControl(self._get_config())
 
+    @dbus.service.method('org.gnome.hamster_dbus')#.hello_world')
+    def hello_world(self):
+        return 'Hello World!'
 
+    # Register signals that can be called by dbus-clients.
+    @dbus.service.signal(DBUS_SERVICE_NAME)
+    def TagsChanged(self): pass
+    def tags_changed(self):
+        self.TagsChanged()
+
+    @dbus.service.signal(DBUS_SERVICE_NAME)
+    def FactsChanged(self): pass
+    def facts_changed(self):
+        self.FactsChanged()
+
+    @dbus.service.signal(DBUS_SERVICE_NAME)
+    def ActivitiesChanged(self): pass
+    def activities_changed(self):
+        self.ActivitiesChanged()
+
+    @dbus.service.signal(DBUS_SERVICE_NAME)
+    def ToggleCalled(self): pass
+    def toggle_called(self):
+        self.toggle_called()
+
+    # General Methods
+    @dbus.service.method(DBUS_SERVICE_NAME)
+    def Quit(self):
+        """Shutdown the service."""
+        #GLib.MainLoop().quit()
+        self.loop.quit()
+
+    # Category Methods
+    @dbus.service.method(DBUS_SERVICE_NAME, in_signature='s', out_signature = 'i')
+    def AddCategory(self, name):
+        """
+        Add a new category.
+
+        Args:
+            name (str): Name of the new category
+
+        Returns:
+            int: PK of the created category or -1 if we failed.
+        """
+
+        # It apears that dbus does *not* enforce passed arguments to be of given
+        # type. In particular, it is quite possible to pass it an integer which
+        # dbus does not even attempt to convert into a string.
+        logger.debug(name)
+        logger.debug(type(name))
+        if not isinstance(name, dbus.String):
+            message = _("Name is not a string!")
+            logger.error(message)
+            result = -1
+        else:
+            name = text_type(name)
+            try:
+                category = Category(name)
+            except:
+                logger.error(sys.exc_info())
+                result = -1
+                raise TypeError
+            else:
+                try:
+                    category = self.controler.store.categories.save(category)
+                except:
+                    logger.error(sys.exc_info())
+                    result = -1
+                    raise ValueError
+                else:
+                    result = category.pk
+                    # [FIXME]
+                    # Why 'activities'?
+                    self.activities_changed()
+        return result
+
+
+
+
+    # Helpers
     def _get_config(self):
         """Get config to be passed to controler."""
         return {
@@ -49,7 +143,7 @@ class HamsterDBusService(dbus.service.Object):
             'fact_min_delta': 60,
             'tmpfile_path': '/tmp/tmpfile.pickle',
             'db_engine': 'sqlite',
-            'db_path': '/tmp/hamster-dbus.sqlite',
+            'db_path': ':memory:',
         }
 
 
@@ -61,6 +155,7 @@ if __name__ == '__main__':
 
     if arg == "server":
         DBusGMainLoop(set_as_default=True)
-        myservice = HamsterDBusService()
+        loop = GLib.MainLoop()
+        myservice = HamsterDBusService(loop=loop)
         # Run needs to be called after we setup our service
-        GLib.MainLoop().run()
+        loop.run()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,119 @@
 import pytest
+import signal
 import subprocess
 import dbus
+import os
+import errno
+import time
 
+# Adaption of testup ised in TtestHamsterDBusServiceDbusMockStyle
+
+@pytest.fixture
+def init_session_bus(request):
+    """
+    Provide a new private session bus so we don't polute the regular one.
+
+    This is a straight copy of: https://github.com/martinpitt/python-dbusmock/blob/master/dbusmock/testcase.py#L92
+
+    Returns:
+        tuple: (pid, address) pair.
+    """
+    def fin():
+        try:
+            del os.environ['DBUS_SESSION_BUS_ADDRESS']
+        except KeyError:
+            pass
+        signal.signal(signal.SIGTERM, signal.SIG_IGN)
+        timeout = 5
+        while timeout > 0:
+            try:
+                os.kill(pid, signal.SIGTERM)
+            except OSError as e:
+                if e.errno == errno.ESRCH:
+                    break
+                else:
+                    raise
+            time.sleep(0.1)
+        else:
+            sys.stderr.write('ERROR: timed out waiting for bus process to terminate\n')
+            os.kill(pid, signal.SIGKILL)
+            time.sleep(0.5)
+        signal.signal(signal.SIGTERM, signal.SIG_DFL)
+
+    argv = ['dbus-launch']
+    out = subprocess.check_output(argv, universal_newlines=True)
+    variables = {}
+    for line in out.splitlines():
+        (k, v) = line.split('=', 1)
+        variables[k] = v
+    pid = int(variables['DBUS_SESSION_BUS_PID'])
+    request.addfinalizer(fin)
+    return (pid, variables['DBUS_SESSION_BUS_ADDRESS'])
 
 
 @pytest.fixture
-def subprocess_service():
-    """Provide a running service fixture using the ``subpprocess`` approach."""
+def session_bus(init_session_bus):
+    """
+    Provide the session bus instance.
+
+    Adapted from: https://github.com/martinpitt/python-dbusmock/blob/master/dbusmock/testcase.py#L137
+    """
+    if os.environ.get('DBUS_SESSION_BUS_ADDRESS'):
+        return dbus.bus.BusConnection(os.environ['DBUS_SESSION_BUS_ADDRESS'])
+    else:
+        return dbus.SessionBus()
+
+
+@pytest.fixture
+def hamster_service(request, session_bus):
+    """
+    Provide a hamster service running as a seperate process.
+
+    This is heavily inspired by the way ``dbusmock`` sets up its ``mock_server``
+    """
+    import subprocess
+    import sys
+    import time
+    import psutil
+    def fin():
+        os.kill(deamon.pid, signal.SIGKILL)
     env = os.environ.copy()
-    subprocess.Popen(['python', 'hamster_dbus/hamster_dbus.py', 'server'], env=env)
-    time.sleep(1)
-    print(subprocess)
+    deamon = subprocess.Popen([sys.executable, '-m', 'hamster_dbus.hamster_dbus', 'server'], env=env)
+    # Wait for the service to become available
+    time.sleep(2)
+    request.addfinalizer(fin)
+    return deamon
 
 
 @pytest.fixture
-def subprocess_remote_service(subprocess_service):
-    bus = dbus.SessionBus()
-    remote_object = bus.get_object('org.gnome.hamster_dbus', '/org/gnome/hamster_dbus')
-    return remote_object
+def hamster_interface(request, session_bus, hamster_service):
+    """Provide a covinient interface hook to our hamster-dbus service."""
+    return session_bus.get_object('org.gnome.hamster_dbus', '/org/gnome/hamster_dbus')
 
 
+
+
+
+# END
+
+
+
+#@pytest.fixture
+#def subprocess_service():
+#    """Provide a running service fixture using the ``subpprocess`` approach."""
+#    env = os.environ.copy()
+#    subprocess.Popen(['python', 'hamster_dbus/hamster_dbus.py', 'server'], env=env)
+#    time.sleep(1)
+#    print(subprocess)
+#
+#
+#@pytest.fixture
+#def subprocess_remote_service(subprocess_service):
+#    bus = dbus.SessionBus()
+#    remote_object = bus.get_object('org.gnome.hamster_dbus', '/org/gnome/hamster_dbus')
+#    return remote_object
+#
+#
 #@pytest.yield_fixture()
 #def mock_loop():
 #    DBusGMainLoop(set_as_default=True)
