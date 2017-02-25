3bdbc9e8c98c969763f6b0048240acbb47e8c5b5
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2017-02-25 14:50:40 +0100

    Add basic object classes and serialization helpers
    We split up the service launch from the actual dbus object definition
    (``objects.py``). This commit also provide an initial go at dedicated
    "serialization" helpers that can convert so called "DbusTuple" (which
    are just regular named tuples storing all relevant data in data
    structures usable via dbus) to and from our ``hamster_lib.objects``
    classes.
    
    ``dbus_to_hamster_*`` serialization helpers accept either regular tuples
    (which allows just dumping return values from dbus methods) as well as
    our specific ``DBusCategory`` (etc.) classes.
    
    This commit does not cover all ``hamster_lib.storage`` methods we may wish to
    export via dbus but should provide a good starting point.
    
    I opted to export each ``*Manager`` as a seperate object with it's own
    interface in order to seperate the codebase as well as make access to
    specific aspecs more clear.
    
    The current code layout is intended to be useable by a dedicated stand
    alone service as well as to be utilizable by 3rd party code as a
    library.

--- hamster_dbus/helpers.py
+++ hamster_dbus/helpers.py
@@ -14,14 +14,32 @@
 # You should have received a copy of the GNU General Public License
 # along with 'hamster-dbus'.  If not, see <http://www.gnu.org/licenses/>.
 
+from calendar import timegm
+import datetime
 from collections import namedtuple
 
-DBusFact = namedtuple('DBusFact', ('pk', 'start', 'end', 'description', 'activity_name',
-    'activity_pk', 'category_name', 'tags', 'date', 'delta'))
+import hamster_lib
 
 
+DBusCategory = namedtuple('DBusCategory', ('pk', 'name'))
+# 'category' is supposed to store an ``DBushamster_lib.Category`` instance.
+DBusActivity = namedtuple('DBusActivity', ('pk', 'name', 'category', 'deleted'))
+# 'activity' is supposed to store an ``DBushamster_lib.Activity`` instance.
+DBusFact = namedtuple('DBusFact', ('pk', 'start', 'end', 'description', 'activity', 'tags'))
 
-def _get_config(self):
+def _none_to_int(value):
+    if value is None:
+        value = -1
+    return value
+
+
+def _int_to_none(value):
+    if value == -1:
+        value = None
+    return value
+
+
+def get_config():
     """Get config to be passed to controler."""
     return {
         'store': 'sqlalchemy',
@@ -37,18 +55,18 @@ def _represent_category(category, legacy_mode=False):
         Return a string representation of a retrieved category that can be returned via dbus.
 
         Args:
-            category (hamsterlib.Category): Category instance or None.
+            category (hamsterlib.hamster_lib.Category): hamster_lib.Category instance or None.
             fallback (str): String to represent ``category=None``.
 
         Returns:
-            str: Name of the passed ``Category`` or ``empty_string`` to indicate
+            str: Name of the passed ``hamster_lib.Category`` or ``empty_string`` to indicate
                 that ``category=None``.
 
         Quite often we will end up with a situation where we need to check whether a
         category is ``None`` or is a proper instance with a name. In order to avoid
         code duplication this is done once in this function.
         Because the representation logic is specific to our dbus requirements we
-        do not simply adjust ``hamsterlib.Category.__str__``.
+        do not simply adjust ``hamsterlib.hamster_lib.Category.__str__``.
 
         Legacy hamster does this on the db level by using ``coalesce`` to assign
         fallback value (``unsorted_localized``). As we opt to leave handling
@@ -70,59 +88,182 @@ def _represent_category(category, legacy_mode=False):
         return result
 
 
-def _hamster_to_dbus_fact(self, fact):
-    """
-    Perform the conversion between Fact-instance and dbus supported data types.
 
-    As far as we can tell of now a dbus-fact looks has the signature
-    of 'iiissisasii' with:
-            i  pk
-            i  start_time
-            i  end_time
-            s  description
-            s  activity name
-            i  activity pk
-            s  category name
-            as List of fact tags
-            i  date (timestamp?)
-            i  delta (format?)
-    """
+# This is needed because not all types used in ``as_tuple`` can be passed
+# through dbus
+def hamster_to_dbus_category(category):
+    def get_pk(category):
+        return _none_to_int(category.pk)
 
-    # Convert our proper datatypes into something dbus can handle.
-    if fact.end:
-        end = timegm(fact.end.timetuple())
-    else:
-        end = 0
+    def get_name(category):
+        return category.name
 
-    if fact.description:
-        description = fact.description
-    else:
-        description = ''
-
-    if fact.activity.category:
-        category_name = fact.activity.category.name
-    else:
-        category_name = _get_unsorted_localized()
-
-    if fact.delta:
-        delta = fact.delta.total_second()
-    else:
-        delta = 0
-
-    return DBusFact(
-        pk=fact.id,
-        start=timegm(fact.start.timetuple()),
-        end=end,
-        description=description,
-        activity_name=fact.activity.name,
-        activity_pk=fact.activity.pk,
-        category_name=category_name,
-        # placeholer for tags
-        tags=dbus.Array(['foo', 'bar'], signature='s'),
-        date=date,
-        delta=delta
+    return DBusCategory(
+        pk=get_pk(category),
+        name=get_name(category)
     )
 
 
-def _get_unsorted_localized():
-    return 'unsorted localized'
+def dbus_to_hamster_category(category_tuple):
+    def get_pk(category_tuple):
+        pk = category_tuple.pk
+        return _int_to_none(pk)
+
+    def get_name(category_tuple):
+        return category_tuple.name
+
+    category_tuple = DBusCategory(*category_tuple)
+    return hamster_lib.Category(
+        pk=get_pk(category_tuple),
+        name=get_name(category_tuple)
+    )
+
+
+def hamster_to_dbus_activity(activity):
+    # (activity.pk, activity.name, (category.pk, category.name),
+    # activity.deleted)
+    # If activity.category=None we need to encode this information in a way that
+    # accomodates dbus type constrains. We can not simply pass None. In order to
+    # distinguish this case from categories that have just not yet recieved a pk
+    # we use ``-2``.
+    def get_pk(activity):
+        pk = activity.pk
+        return _none_to_int(pk)
+
+    def get_name(activity):
+        return activity.name
+
+    def get_category(activity):
+        if activity is None:
+            category = (-2, '')
+        else:
+            category = hamster_to_dbus_category(activity.category)
+        return category
+
+    def get_deleted(activity):
+        return activity.deleted
+
+    return DBusActivity(
+        pk=get_pk(activity),
+        name=get_name(activity),
+        category=get_category(activity),
+        deleted=get_deleted(activity)
+    )
+
+
+def dbus_to_hamster_activity(activity_tuple):
+    """
+    Convert an 'DBus activity tuple to activity.
+
+    Returns:
+        hamster_lib.Activity: hamster_lib.Activity instance.
+    """
+    def get_pk(activity_tuple):
+        return _int_to_none(activity_tuple.pk)
+
+    def get_name(activity_tuple):
+        return activity_tuple.name
+
+    def get_category(activity_tuple):
+        category_tuple = activity_tuple.category
+        return dbus_to_hamster_category(category_tuple)
+
+    activity_tuple = DBusActivity(*activity_tuple)
+
+    return hamster_lib.Activity(
+        pk=get_pk(activity_tuple),
+        name=get_name(activity_tuple),
+        category=get_category(activity_tuple),
+    )
+
+# Note that we currently do not support passing timezone information!
+def hamster_to_dbus_fact(fact):
+    """
+    Perform the conversion between hamster_lib.Fact-instance and dbus supported data types.
+
+    (iiis(is(is)b)a(is))':
+    i           pk: -1 -> None
+    s           start
+    s           end
+    s           description
+    (is(is)b)   activity tuple: (pk, name, category_tuple).
+    a(is)       list of tag tuples: (pk, name). pk=-1 -> None
+    """
+
+    def get_pk(fact):
+        return _none_to_int(fact.pk)
+
+    def get_start(fact):
+        if fact.start:
+            start = fact.start.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            start = ''
+        return start
+
+    def get_end(fact):
+        if fact.end:
+            end = fact.end.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            end = ''
+        return end
+
+    def get_activity(fact):
+        activity = fact.activity
+        return hamster_to_dbus_activity(activity)
+
+    def get_description(fact):
+        return fact.description
+
+    def get_tags(fact):
+        # [FIXME]
+        return []
+
+    return DBusFact(
+        pk=get_pk(fact),
+        start=get_start(fact),
+        end=get_end(fact),
+        activity=get_activity(fact),
+        # [FIXME]
+        tags=get_tags(fact),
+        description=get_description(fact),
+    )
+
+
+def dbus_to_hamster_fact(fact_tuple):
+    def get_pk(fact_tuple):
+        return _int_to_none(fact_tuple.pk)
+
+    def get_start(fact_tuple):
+        if fact_tuple.start:
+            start = datetime.datetime.strptime(fact_tuple.start, '%Y-%m-%d %H:%M:%S')
+        else:
+            start = None
+        return start
+
+    def get_end(fact_tuple):
+        if fact_tuple.end:
+            end = datetime.datetime.strptime(fact_tuple.end, '%Y-%m-%d %H:%M:%S')
+        else:
+            end = None
+        return end
+
+    def get_activity(fact_tuple):
+        return dbus_to_hamster_activity(fact_tuple.activity)
+
+    def get_tags(fact_tuple):
+        return []
+
+    def get_description(fact_tuple):
+        return fact_tuple.description
+
+    # Make sure we can deal with proper DBusFact instances as well as with
+    # 'unprepared' normal tuples.
+    fact_tuple = DBusFact(*fact_tuple)
+    return hamster_lib.Fact(
+        pk=get_pk(fact_tuple),
+        start=get_start(fact_tuple),
+        end=get_end(fact_tuple),
+        activity=get_activity(fact_tuple),
+        tags=get_tags(fact_tuple),
+        description=get_description(fact_tuple)
+    )
---
+++ hamster_dbus/objects.py
@@ -0,0 +1,371 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of 'hamster-dbus'.
+#
+# 'hamster-dbus' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'hamster-dbus' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with  'hamster-dbus'.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+A basic dbus service that exposes a subset up hamster-lib.
+
+Most of the methods exposed here are just 'proxies' for the corresponding
+``hamster-lib.storage``-manager methods. For details on arguments and semantics
+please also refer to their documentation as we want to avoid duplication).
+
+Capitalization violates PEP8 due to dbus specifications on method names
+(https://dbus.freedesktop.org/doc/dbus-api-design.html).
+"""
+# I still do not understand dbus objects completely so for now there is only the
+# main ``HamsterDBus`` object. Object specific managers are implemented as
+# separate interfaces to provide as least some degree of grouping/namespaceing.
+# Currently hamster-lib objects need to be converted into something that can be
+# passed over dbus. Future iteration should revisit this and see if we can
+# expose those objects as dbus objects and consequently just pass those.
+
+from __future__ import unicode_literals, absolute_import
+
+import hamster_lib
+import dbus
+import dbus.service
+
+from . import helpers
+
+
+
+DBUS_CATEGORIES_INTERFACE = 'org.projecthamster.HamsterDBus.CategoryManager1'
+DBUS_ACTIVITIES_INTERFACE = 'org.projecthamster.HamsterDBus.ActivityManager1'
+DBUS_FACTS_INTERFACE = 'org.projecthamster.HamsterDBus.FactManager1'
+
+
+def _get_dbus_bus_name():
+    """Return the bus name."""
+    # We wrap this in a function instead of a constant to avoid instant
+    # instantiation if we use this module as a library.
+    return dbus.service.BusName(
+        name='org.projecthamster.HamsterDBus',
+        bus=dbus.SessionBus()
+    )
+
+
+class HamsterDBus(dbus.service.Object):
+    """A dbus object providing access to general hamster-lib capabilities."""
+
+    # [FIXME]
+    # 'controller still needed?
+    def __init__(self, loop):
+        self._loop = loop
+
+        super(HamsterDBus, self).__init__(
+            bus_name=_get_dbus_bus_name(),
+            object_path='/org/projecthamster/HamsterDBus',
+        )
+
+    @dbus.service.method('org.projecthamster.HamsterDBus')
+    def Quit(self):
+        """Shutdown the service."""
+        self._loop.quit()
+
+
+class CategoryManager(dbus.service.Object):
+
+    def __init__(self, controller):
+        self._controller = controller
+
+        super(CategoryManager, self).__init__(
+            bus_name=_get_dbus_bus_name(),
+            object_path='/org/projecthamster/HamsterDBus/CategoryManager',
+        )
+
+    # [FIXME]
+    # Missing ``hamster_lib.CategoryManager`` methods that still need to be implemented:
+    # ``get_or_create``.
+
+    @dbus.service.method(DBUS_CATEGORIES_INTERFACE, in_signature='(is)', out_signature = '(is)')
+    def Save(self, category_tuple):
+        """
+        Save category.
+
+        Args:
+            category_tuple: hamster_lib.Category tuple.
+
+        Returns:
+            tuple: ``(category.pk, category.name)`` tuple.
+        """
+        category = helpers.dbus_to_hamster_category(category_tuple)
+        category = self._controller.store.categories.save(category)
+        return helpers.hamster_to_dbus_category(category)
+
+    @dbus.service.method(DBUS_CATEGORIES_INTERFACE, in_signature='i')
+    def Remove(self, pk):
+        """
+        Remove a category.
+
+        Args:
+            pk (int): PK of the category to be removed.
+
+        Returns:
+            None: Nothing.
+        """
+        # [TODO]
+        # Once LIB-239 has been solved, we should be able to skip extra category
+        # retrieval.
+        category = self._controller.store.categories.get(pk)
+        self._controller.store.categories.remove(category)
+        return None
+
+    @dbus.service.method(DBUS_CATEGORIES_INTERFACE, in_signature='s', out_signature='(is)')
+    def GetByName(self, name):
+        """
+        Look up a category by its name and return its PK.
+
+        Args:
+            name (str): Name of the category to we want the PK of.
+
+        Returns:
+            tuple: hamster_lib.Category tuple.
+        """
+        category = self._controller.categories.get_by_name(name)
+        return helpers.hamster_to_dbus_category(category)
+
+    @dbus.service.method(DBUS_CATEGORIES_INTERFACE, out_signature='a(is)')
+    def GetAll(self):
+        """
+        Get all categories.
+
+        Returns:
+            list: List of tuples with (category.pk, category.name)
+        """
+        categories = self._controller.categories.get_all()
+        return [helpers.hamster_to_dbus_category(category) for category in categories]
+
+
+class ActivityManager(dbus.service.Object):
+
+    def __init__(self, controller):
+        self._controller = controller
+
+        super(ActivityManager, self).__init__(
+            bus_name=_get_dbus_bus_name(),
+            object_path='/org/projecthamster/HamsterDBus/ActivityManager',
+        )
+
+    @dbus.service.method(DBUS_ACTIVITIES_INTERFACE, in_signature='(is(is)b)', out_signature='(is(is)b)')
+    def Save(self, activity_tuple):
+        """
+        Save an activity.
+
+        Args:
+            activity_tuple (tuple): Tuple with activity values.
+
+        Returns:
+            tuple: Tuple representing the saved activity.
+        """
+        activity = helpers.dbus_to_hamster_activity(activity_tuple)
+        result = self._controller.activities.save(activity)
+
+        # [FIXME]
+        #self.activities_changed()
+        return helpers.hamster_to_dbus_activity(result)
+
+    @dbus.service.method(DBUS_ACTIVITIES_INTERFACE, in_signature='i')
+    def Remove(self, pk):
+        """Remove an activity.
+
+        Args:
+            pk (int): PK of the activity to be removed.
+
+        Returns:
+            None: Nothing.
+        """
+        activity = self._controller.activities.get(pk)
+        self._controller.activities.remove(activity)
+
+        # [FIXME]
+        #self.activities_changed()
+        return None
+
+    @dbus.service.method(DBUS_ACTIVITIES_INTERFACE, in_signature='i', out_signature='(is(is)b)')
+    def Get(self, pk):
+        """
+        Retrieve an ``hamster_lib.Activity`` based on it's PK.
+
+        Args:
+            pk (int): PK of the activity to be fetched.
+
+        Returns:
+            tuple: hamster_lib.Activity tuple
+        """
+        activity = self._controller.store.activities.get(pk)
+        return helpers.hamster_to_dbus_activity(activity)
+
+    @dbus.service.method(DBUS_ACTIVITIES_INTERFACE, in_signature='i',
+        out_signature='a(is(is)b)')
+    def GetAll(self, category_pk):
+        """
+        Retrieve all ``hamster_lib.Activity`` instances that match the criteria.
+
+        Args:
+            category_pk (int): hamster_lib.Category pk. Use ``-1`` for ``None`` and ``-2`` for ``False``.
+                Refer to ``hamster_lib.storage.hamster_lib.ActivityManager.get_all`` for details.
+
+        Returns:
+            tuple: (activity_tuple, error).
+        """
+        if category_pk == -1:
+            category = None
+        elif category_pk == -2:
+            category = False
+        else:
+            category = self._controller.store.categories.get(category_pk)
+
+        activities = self._controller.store.activities.get_all(category)
+
+        return [helpers.hamster_to_dbus_activity(activity) for activity in activities]
+
+
+class FactManager(dbus.service.Object):
+
+    def __init__(self, controller):
+        self._controller = controller
+
+        super(FactManager, self).__init__(
+            bus_name=_get_dbus_bus_name(),
+            object_path='/org/projecthamster/HamsterDBus/FactManager',
+        )
+
+    @dbus.service.method(DBUS_FACTS_INTERFACE, in_signature='s', out_signature='(isss(is(is)b)a(is))')
+    def SaveRaw(self, raw_fact):
+        """
+        Take a raw_fact save it to our backend.
+
+        Args:
+            raw_fact (str): ``raw fact`` string.
+
+        Returns:
+            tuple (DBushamster_lib.Fact): Serialized version of the saved ``hamster_lib.Fact`` instance.
+
+        Note: This method is identical to ``Savehamster_lib.Fact`` with the only difference being
+            that it takes a ``raw fact`` instead of a serialized ``hamster_lib.Fact`` instance.
+        """
+        fact = hamster_lib.Fact.create_from_raw_fact(raw_fact)
+        result = self._controller.store.facts.save(fact)
+
+        # [FIXME]
+        # self.facts_changed()
+
+        return helpers.hamster_to_dbus_fact(result)
+
+    @dbus.service.method(DBUS_FACTS_INTERFACE, in_signature='(isss(is(is)b)a(is))', out_signature='(isss(is(is)b)a(is))')
+    def Save(self, fact_tuple):
+        """
+        Take a fact save it to our backend.
+
+        Args:
+            fact_tuple (DBushamster_lib.Fact): ``hamster_lib.Fact`` to be saved.
+
+        Returns:
+            tuple (DBushamster_lib.Fact): Serialized version of the saved ``hamster_lib.Fact`` instance.
+        """
+        fact = helpers.dbus_to_hamster_fact(fact_tuple)
+        result = self._controller.store.facts.save(fact)
+
+        # [FIXME]
+        # self.facts_changed()
+
+        return helpers.hamster_to_dbus_fact(result)
+
+    @dbus.service.method(DBUS_FACTS_INTERFACE, in_signature='i')
+    def Remove(self, fact_pk):
+        """
+        Remove fact from storage by it's PK
+
+        Args:
+            fact_pk (int): PK of the fact to be removed.
+
+        Returns:
+            None: Nothing.
+        """
+        fact = self._controller.store.facts.get(fact_pk)
+        result = self._controller.store.facts.remove(fact)
+        fact = self._controller.store.facts.get(fact_pk)
+
+        # [FIXME]
+        #if result:
+        #    self.facts_changed()
+        return None
+
+    @dbus.service.method(DBUS_FACTS_INTERFACE, in_signature='i', out_signature='(isss(is(is)b)a(is))')
+    def Get(self, fact_pk):
+        """Get fact by id..
+
+        Args:
+            fact_pk (int): PK of the fact to be retrieved.
+
+        Returns:
+            DBushamster_lib.Fact: Serialized ``hamster_lib.Fact`` instance.
+        """
+
+        fact = self._controller.facts.get(fact_pk)
+        return helpers.hamster_to_dbus_fact(fact)
+
+    @dbus.service.method(DBUS_FACTS_INTERFACE, in_signature='sss',
+        out_signature='a(isss(is(is)b)a(is))')
+    def GetAll(self, start, end, filter_term):
+        """
+        Gets all facts matching criteria.
+
+        Args:
+            start (int): Unix-timestamp for start of timeframe. ``-1`` for ``None``.
+            end (int): Unix-timestamp for end of timeframe. ``-1`` for ``None``.
+            filter_term (str): Only consider ``hamster_lib.Facts`` with this string as part of their
+                associated ``hamster_lib.Activity.name``
+
+        Returns:
+            list: A list of ``helpers.DBushamster_lib.Fact``-tuples.
+                For details on those, please see ``helpers.hamster_to_dbus_fact``.
+
+        Note:
+            Our ``hamsterlib`` manager method is actually more flexible and would allow
+            for ``datetime.datetime`` or ``datetime.time`` instances instead of just
+            ``datetime.date`` instances for ``start`` and ``end``. But for compatibility
+            reasons we stick with the simpler legacy version for now.
+        """
+        def get_start(start):
+            return None
+
+        def get_end(end):
+            return None
+
+        def get_filter_term(end):
+            return filter_term
+
+        facts = self._controller.store.facts.get_all(get_start(start), get_end(end),
+            get_filter_term(filter_term))
+        return [helpers.hamster_to_dbus_fact(fact) for fact in facts]
+
+    @dbus.service.method(DBUS_FACTS_INTERFACE, out_signature='a(isss(is(is)b)a(is))')
+    def GetTodays(self):
+        """
+        Gets facts of today, respecting hamster day_start, day_end settings.
+
+        Returns:
+            list: A list of ``helpers.DBushamster_lib.Fact``-tuples.
+                For details on those, please see ``helpers.DBushamster_lib.Fact``.
+
+        Note:
+            This only returns proper facts and will not include any ongoing fact!
+        """
+        facts = self.controller.store.facts.get_today()
+        return [helpers.dbus_to_hamster_fact(fact) for fact in facts]
+
